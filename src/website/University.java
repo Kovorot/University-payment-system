6d5d156e07959ef6f25fe3ae5948ae8b9f003f0f
Andrey Vasilev <kovorot0875@mail.ru>
2023-02-17 00:39:39 +0300

    university web-site added

---
+++ src/website/University.java
@@ -0,0 +1,12 @@
+package website;
+
+public class University {
+
+    public static void printGreetings() throws Exception {
+        System.out.println("Добро пожаловать на сайт университета!");
+        Thread.sleep(1000);
+        System.out.println("Выберите действие:");
+        Thread.sleep(1000);
+        System.out.println("1. Оплатить обучение");
+    }
+}
